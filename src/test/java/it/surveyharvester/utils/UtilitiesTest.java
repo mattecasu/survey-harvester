358fb63d719fc9cbb363c4747f6d96be5887210a
mattecasu <mattecasu@gmail.com>
2015-07-05 02:36:00 +0200

    enriched test

--- src/test/java/it/surveyharvester/utils/UtilitiesTest.java
+++ src/test/java/it/surveyharvester/utils/UtilitiesTest.java
@@ -1,15 +1,23 @@
 package it.surveyharvester.utils;
 
+import static com.google.common.collect.Lists.newArrayList;
 import static it.surveyharvester.utils.Utilities.extractListFromString;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 
-import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
-import com.google.common.collect.Lists;
-
 public class UtilitiesTest {
+
+    List<String> listToCheck;
+
+    @Before
+    public void setup() {
+        listToCheck = newArrayList("a", "b", "c", "d");
+    }
 
     @Test
     public void test() {
@@ -17,11 +25,12 @@ public class UtilitiesTest {
         List<String> extracted2 = extractListFromString("a,b,c , d", ", ");
         List<String> extracted3 = extractListFromString("a,b,c,d", ",");
         List<String> extracted4 = extractListFromString("a,b,c;d", ", ");
-        
-        Assert.assertTrue(extracted1.containsAll(Lists.newArrayList("a", "b", "c", "d")));
-        Assert.assertTrue(extracted2.containsAll(Lists.newArrayList("a", "b", "c", "d")));
-        Assert.assertTrue(extracted3.containsAll(Lists.newArrayList("a", "b", "c", "d")));
-        
-        Assert.assertFalse(extracted4.containsAll(Lists.newArrayList("a", "b", "c", "d")));
+
+        assertTrue(extracted1.containsAll(listToCheck));
+        assertTrue(extracted2.containsAll(listToCheck));
+        assertTrue(extracted3.containsAll(listToCheck));
+
+        assertFalse(extracted4.containsAll(listToCheck));
+        assertTrue(extracted4.containsAll(newArrayList("a", "b", "c;d")));
     }
 }
